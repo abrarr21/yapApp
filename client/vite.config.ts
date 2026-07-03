249a5cfb972a7abea2a349987c11bac6f5902417
abrarr21 <abrarrr2110@gmail.com>
2026-07-03 10:15:34 +0530

    chore: dockerize application with Docker Compose

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,4 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), babel({ presets: [reactCompilerPreset()] })],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
 });
